ca6357f96b9dd692b05f8e12e2d56e65c28a4c0f
unknown <giancarloestrella09@gmail.com>
2021-11-20 22:15:07 +0800

    Sorts 4 given digits from highest to lowest.

--- 1-numbersorter.py
+++ 1-numbersorter.py
@@ -7,5 +7,8 @@ first_num = float(input("Enter your first number here: "))
 sec_num = float(input("Enter your second number here: "))
 third_num = float(input("Enter your third number here: "))
 fourth_num = float(input("Enter your last number here: "))
+
 # 2. sort from highest to lowest
+highest = max(first_num, sec_num, third_num, fourth_num) # find the highest number first
+
 # 3. display order
