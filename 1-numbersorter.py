c04f373bd41618ccde90df9d35baa35f84559742
unknown <giancarloestrella09@gmail.com>
2021-11-20 22:54:54 +0800

    Sorts 4 given digits from highest to lowest.

--- 1-numbersorter.py
+++ 1-numbersorter.py
@@ -11,4 +11,20 @@ fourth_num = float(input("Enter your last number here: "))
 # 2. sort from highest to lowest
 highest = max(first_num, sec_num, third_num, fourth_num) # find the highest number first
 
+def Order():
+    if highest == first_num:
+        if sec_num > third_num and third_num > fourth_num:
+            print(f"The order of the set of numbers are as follows: {first_num}, {sec_num}, {third_num}, and {fourth_num}.")
+        elif sec_num < third_num and sec_num > fourth_num:
+            print(f"The order of the set of numbers are as follows: {first_num}, {third_num}, {sec_num}, and {fourth_num}.")
+        elif sec_num < fourth_num and fourth_num > sec_num:
+            print(f"The order of the set of numbers are as follows: {first_num}, {third_num}, {fourth_num}, and {sec_num}.")
+        elif third_num < fourth_num and third_num > sec_num:
+            print(f"The order of the set of numbers are as follows: {first_num}, {fourth_num}, {third_num}, and {sec_num}.")
+        elif sec_num < fourth_num and sec_num > third_num:
+            print(f"The order of the set of numbers are as follows: {first_num}, {fourth_num}, {sec_num}, and {third_num}.")
+        else:
+            print(f"The order of the set of numbers are as follows: {first_num}, {sec_num}, {fourth_num}, and {third_num}.")
+
 # 3. display order
+display = Order()
