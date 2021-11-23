acaadeb3acbf0d98d210b1b673d90ced554a8fa3
unknown <giancarloestrella09@gmail.com>
2021-11-23 22:45:21 +0800

    Gives 10 sets of addition problems, checks answers, and gives the final score.

--- 2-additiontrainer.py
+++ 2-additiontrainer.py
@@ -4,15 +4,19 @@
 # Display the result summary of the 10 operations (ex 9/10)
 
 from random import randrange
+player = input("Please enter your name here: ")
+
+print("-" * 24)
+
+print(f"Welcome to the quiz, {player}!")
+print("What are we waiting for? Let's start the quiz!")
+
+print("-" * 24)
+
 title = "** A Simple Addition Quiz **"
 print("*" * len(title))
 print(title)
 print("*" * len(title))
-
-player = input("Please enter your name here: ")
-
-print(f"Welcome to the quiz, {player}!")
-print("What are we waiting for? Let's start the quiz!")
 
 MaxScore = 10
 CurrentScore = 0
@@ -31,6 +35,9 @@ while count <= 10:
     else:
         print(f"Your answer is incorrect! The right answer is {sum}.")
     count = count + 1
+
+print("-" * 24)
+
 print(f"You got {CurrentScore} out of {MaxScore} questions correct.")
 if CurrentScore == 10:
     print(f"Congratulations, {player}! You have mastered adding numbers!")
@@ -40,3 +47,7 @@ else:
     if CurrentScore <= 5:
         print(f"You failed the quiz, {player}! Try to focus on this lesson first.")
         print("Better luck next time!")
+
+print("-" * 24)
+
+print("Thank you for taking this quiz.")
